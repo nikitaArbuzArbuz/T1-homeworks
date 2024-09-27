71b227c9eb2ec6dbb8a47344fd729cfdf4964756
Nikita <talker_64@vk.com>
2024-09-27 18:07:13 +0300

    add - create new topic kafka

---
+++ README.md
@@ -0,0 +1,11 @@
+Подключение kafka и проверка, что работает
+
+![kafka.jpg](screenshots%2Fkafka.jpg)
+
+Создание нового топика
+
+![new_topic.jpg](screenshots%2Fnew_topic.jpg)
+
+Отправка тестовых данных с помощью java инструментов
+
+![new_datas_of_topic.jpg](screenshots%2Fnew_datas_of_topic.jpg)
--- src/main/java/ru/t1/java/demo/controller/ClientController.java
+++ src/main/java/ru/t1/java/demo/controller/ClientController.java
@@ -4,6 +4,8 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import ru.t1.java.demo.aop.HandlingResult;
 import ru.t1.java.demo.aop.LogException;
@@ -30,9 +32,12 @@ public class ClientController {
     @HandlingResult
     public void parseSource() {
         List<ClientDto> clientDtos = clientService.parseJson();
-        clientDtos.forEach(dto -> {
-            kafkaClientProducer.sendTo(topic, dto);
-        });
+        clientDtos.forEach(dto -> kafkaClientProducer.sendTo(topic, dto));
     }
 
+    @PostMapping("/add_kafka")
+    @HandlingResult
+    public void addToKafka(@RequestBody ClientDto clientDto) {
+        kafkaClientProducer.sendTo(topic, clientDto);
+    }
 }
