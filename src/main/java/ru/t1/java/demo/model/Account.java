3680bcb0e3b2a2c6a524e8eb3cc6a3bd2db4b4d6
Nikita <talker_64@vk.com>
2024-09-28 22:07:19 +0300

    kafka_02: add - entity Account

---
+++ src/main/java/ru/t1/java/demo/model/Account.java
@@ -0,0 +1,28 @@
+package ru.t1.java.demo.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.jpa.domain.AbstractPersistable;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "account")
+public class Account extends AbstractPersistable<Long> {
+    @Column(name = "account_type")
+    @Enumerated(value = EnumType.STRING)
+    private AccountType accountType;
+
+    @Column(name = "balance", precision = 19, scale = 2)
+    private BigDecimal balance;
+
+    public enum AccountType {
+        DEBIT,
+        CREDIT
+    }
+}
